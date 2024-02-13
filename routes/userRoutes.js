578192cabbb80f4a859a8276e9152aaba72a6ae5
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-13 12:41:56 +0530

    feat: add isAuth middleware to protect api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,65 +3,78 @@ const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const tokenGenerator = require("../lib/tokenGenerator");
+const isAuth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get(
   "/",
+  isAuth,
   asyncHandler(async (req, res) => {
-    const searchTerm = req.query.search;
-    const query = {
-      $or: [
-        { username: { $regex: searchTerm, $options: "i" } }, // Case-insensitive regex for name
-        { email: { $regex: searchTerm, $options: "i" } }, // Case-insensitive regex for email
-      ],
-    };
+    try {
+      const searchTerm = req.query.search;
+      const query = {
+        $or: [
+          { username: { $regex: searchTerm, $options: "i" } },
+          { email: { $regex: searchTerm, $options: "i" } },
+        ],
+      };
 
-    // Use the User model to find matching users
-    // const users = await User.find(query);
+      // const users = await User.find(query);
+      const users = await User.find(query).find({
+        _id: { $ne: req.user._id },
+      });
 
-    // Respond with the found users
-    res.json(query);
+      res.json(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
   })
 );
 
 router.post(
   "/register",
   asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
+    try {
+      const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-      res.status(400).json({ message: "Please fill in all fields" });
-      throw new Error("Please fill in all fields");
-    }
+      if (!username || !email || !password) {
+        res.status(400).json({ message: "Please fill in all fields" });
+        throw new Error("Please fill in all fields");
+      }
 
-    const userExists = await User.findOne({ email });
+      const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      res.status(400).json({ message: "User already exists" });
-      throw new Error("User already exists");
-    }
+      if (userExists) {
+        res.status(400).json({ message: "User already exists" });
+        throw new Error("User already exists");
+      }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        password: user.password,
-        token: tokenGenerator(user._id),
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
       });
-    } else {
-      res.status(400).json({ message: "Invalid user data" });
-      throw new Error("Invalid user data");
+
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          password: user.password,
+          token: tokenGenerator(user._id),
+        });
+      } else {
+        res.status(400).json({ message: "Invalid user data" });
+        throw new Error("Invalid user data");
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Server error", error: error.message });
     }
   })
 );
@@ -69,21 +82,27 @@ router.post(
 router.post(
   "/login",
   asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+      const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: tokenGenerator(user._id),
-      });
-    } else {
-      res.status(401).json({ message: "Invalid email or password" });
-      throw new Error("Invalid email or password");
+      if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: tokenGenerator(user._id),
+        });
+      } else {
+        res.status(401).json({ message: "Invalid email or password" });
+        throw new Error("Invalid email or password");
+      }
+    } catch (error) {
+      console.log(error);
+
+      res.status(500).json({ message: "Server error", error: error.message });
     }
   })
 );
