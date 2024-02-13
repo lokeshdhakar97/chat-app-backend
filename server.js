6df66c41d3dd8a31bd1762c8f6b9c4e5cfb75413
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-13 12:14:52 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messsageRoutes = require('./routes/messageRoutes');
+const connectDB = require('./lib/db')
+
+
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messsageRoutes);
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
