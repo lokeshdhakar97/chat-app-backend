fe9e1589d17968d178e2e0483ef08e158b8538b8
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-19 11:34:37 +0530

    feat: implement socket

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const chatRoutes = require("./routes/chatRoutes");
 const messsageRoutes = require("./routes/messageRoutes");
 const connectDB = require("./lib/db");
 const cors = require("cors");
-const http = require("http");
-const socketIO = require("socket.io");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
 const app = express();
 connectDB();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
@@ -17,25 +24,21 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messsageRoutes);
 
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: { origin: "*" },
-});
-io.on("connection", (socket) => {
-  console.log("Check");
-  console.log("New user connected");
-
-  // socket.on("sendMessage", (message) => {
-  //   io.emit("message", message); // Broadcast the message to all connected clients
-  // });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("New user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("say_hello", (data) => {
+    console.log(data);
+    socket.emit("res", "Everthings Good");
+  });
 });
